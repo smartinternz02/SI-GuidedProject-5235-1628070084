76f9e20479cea729ae520f943cac6d6a9d3a13a5
Saiteja <48819675+saitejakusam@users.noreply.github.com>
2021-08-09 19:59:43 +0530

    Delete src directory

--- src/main/java/com/example/demo/ExpenseService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-import java.util.List;
-
-public interface ExpenseService {
-List<Expense> getAllExpenses();
-List<Expense> getAllExpenseByEmail(String email);
-Expense getExpenseById(long id);
-
-}
--- src/main/java/com/example/demo/userrepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface userrepo extends JpaRepository<User,String>{
-User findByEmail(String email);
-}
